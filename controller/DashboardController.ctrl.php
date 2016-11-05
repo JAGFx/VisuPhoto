a1de43135ac4bea33ec807288f5eea59a6993f95
JAGFx <contact@jagfx.fr>
2016-11-05 13:10:13 +0100

    Feature Dashboard: Nettoyage code inutile

--- controller/DashboardController.ctrl.php
+++ controller/DashboardController.ctrl.php
@@ -14,14 +14,11 @@
 	 * Contrôleur pour le dashboard
 	 */
 	class DashboardController extends Controller {
-		private $_subNav;
-
 		/**
 		 * DashboardController constructor.
 		 */
 		public function __construct() {
 			parent::__construct();
-			$this->setDefaultView( 'Dashboard/base.view.php' );
 		}
 
 		// ---------------------------------------------------------------------------------------------- Actions
@@ -32,39 +29,11 @@
 		 */
 		public function dashboardAction() {
 			if ( UserSessionManager::hasPrivilege( UserSessionManager::USER_PRIVILEGE ) ) {
-				$photoDAO = loadDAO( 'ImageDAO' );
-				$albumDAO = loadDAO( 'AlbumDAO' );
+				$this->makeMenu();
 
-				$this->_dataContent[ 'listeCtge' ]   = $photoDAO->getListCategory();
-				$this->_dataContent[ 'listImg' ]     = $photoDAO->getFullImageList();
-				$this->_dataContent[ 'listImgUser' ] = $albumDAO->findListAlbumByUser(
-					UserSessionManager::getSession()
-				);
-				$this->renderView( __FUNCTION__ );
-
-			} else
-				$this->redirectToRoute( 'loginUser' );
-		}
-
-		public function editionDashboardAction() {
-			if ( UserSessionManager::hasPrivilege( UserSessionManager::USER_PRIVILEGE ) ) {
-				$entity = ( isset( $_GET[ 'e' ] ) && !empty( $_GET[ 'e' ] ) )
-					? htmlentities( $_GET[ 'e' ] )
-					: null;
-				$id     = ( isset( $_GET[ 'i' ] ) && !empty( $_GET[ 'i' ] ) )
-					? htmlentities( $_GET[ 'i' ] )
-					: null;
-
-				$albumDAO = loadDAO( 'AlbumDAO' );
-				$photoDAO = loadDAO( 'ImageDAO' );
-				if ( $entity === 'Album' ) {
-
-					$this->_dataContent[ 'albumToEdit' ] = $albumDAO->findAlbumById( $id );
-					$this->_dataContent[ 'listImg' ]     = $photoDAO->getFullImageList();
-					$this->_dataContent[ 'pathInclude' ] = __DIR__ . '/../view/Album/editAlbum.view.php';
-				}
-
-				$this->renderView( __FUNCTION__ );
+				$this->getViewManager()
+				     ->setPageView( 'Dashboard/base' )
+				     ->render( 'Dashboard/dashboard' );
 
 			} else
 				$this->redirectToRoute( 'loginUser' );
@@ -74,28 +43,5 @@
 		/**
 		 * Génération des données du contenu
 		 */
-		protected function makeContent() {
-			$this->_subNav = [
-				'Album' => [
-					'Liste' => '#listAlbum',
-					'Créer' => '#addAlbum'
-				],
-				'Image' => [
-					'Liste' => '#listImage',
-					'Créer' => '#addImage'
-				]
-			];
-		}
-
-		/**
-		 * Convertis les données de class en un tableau
-		 *
-		 * @return array
-		 */
-		protected function toData() {
-			return [
-				'menu'   => $this->_menu,
-				'subNav' => $this->_subNav
-			];
-		}
+		protected function makeContent() { }
 	}
--- view/Dashboard/dashboard.view.php
+++ view/Dashboard/dashboard.view.php
@@ -1,16 +1,10 @@
 <section class="col-xs-12" >
 	<div class="tab-content" >
 		<div role="tabpanel" class="tab-pane active" id="addImage" >
-			<?php require __DIR__ . '/../Photo/addPhoto.view.php'; ?>
-		</div >
-		<div role="tabpanel" class="tab-pane" id="addAlbum" >
-			<?php require __DIR__ . '/../Album/addAlbum.view.php'; ?>
-		</div >
-		<!--<div role="tabpanel" class="tab-pane" id="editAlbum" >
-			<?php /*require __DIR__ . '/../Album/editAlbum.view.php'; */ ?>
-		</div >-->
-		<div role="tabpanel" class="tab-pane" id="listAlbum" >
-			<?php require __DIR__ . '/../Album/listAlbum.view.php'; ?>
+			<section class="row" >
+				<h1 class="col-xs-12 page-header" >Album > Liste</h1 >
+				plop
+			</section >
 		</div >
 	</div >
 </section >

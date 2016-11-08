14599fc63075b431e6a32409380142770df28aab
JAGFx <contact@jagfx.fr>
2016-11-08 16:51:17 +0100

    Structure : Dépalcement des assets

--- view/Dashboard/base.view.php
+++ view/Dashboard/base.view.php
@@ -8,13 +8,13 @@
 		<meta name="author" content="" >
 		<title >Dashboard Template for Bootstrap</title >
 
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/lib/font-awesome-4.7.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/lib/formsJS/formJS.css" />
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/css/ie10-viewport-bug-workaround.css" />
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/css/dashboard.css" media="screen" title="Normal" />
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/lib/selectize.js-master/dist/css/selectize.css" >
-		<link rel="stylesheet" type="text/css" href="view/Default/assets/lib/selectize.js-master/dist/css/selectize.default.css" >
+		<link rel="stylesheet" type="text/css" href="assets/lib/bootstrap-3.3.7-dist/css/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/lib/font-awesome-4.7.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="assets/lib/formsJS/formJS.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/ie10-viewport-bug-workaround.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/dashboard.css" media="screen" title="Normal" />
+		<link rel="stylesheet" type="text/css" href="assets/lib/selectize.js-master/dist/css/selectize.css" >
+		<link rel="stylesheet" type="text/css" href="assets/lib/selectize.js-master/dist/css/selectize.default.css" >
 	</head >
 
 	<body >
@@ -37,13 +37,13 @@
 			</div >
 		</div >
 
-		<script src="view/Default/assets/lib/jquery-3.1.1/jquery-3.1.1.min.js" ></script >
-		<script src="view/Default/assets/lib/bootstrap-3.3.7-dist/js/bootstrap.min.js" ></script >
-		<script src="view/Default/assets/lib/formsJS/forms.js" ></script >
-		<script src="view/Default/assets/js/ie10-viewport-bug-workaround.js" ></script >
-		<script src="view/Default/assets/js/script.js" ></script >
-		<script src="view/Default/assets/lib/jquery-ui-1.12.1.custom/jquery-ui.min.js" ></script >
-		<script src="view/Default/assets/lib/selectize.js-master/dist/js/selectize.min.js" ></script >
+		<script src="assets/lib/jquery-3.1.1/jquery-3.1.1.min.js" ></script >
+		<script src="assets/lib/bootstrap-3.3.7-dist/js/bootstrap.min.js" ></script >
+		<script src="assets/lib/formsJS/forms.js" ></script >
+		<script src="assets/js/ie10-viewport-bug-workaround.js" ></script >
+		<script src="assets/js/script.js" ></script >
+		<script src="assets/lib/jquery-ui-1.12.1.custom/jquery-ui.min.js" ></script >
+		<script src="assets/lib/selectize.js-master/dist/js/selectize.min.js" ></script >
 
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js" ></script >
